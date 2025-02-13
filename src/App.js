f554b0d0e01c7597377ca6fde155d49a948e211f
kambagiri <kambagiri09@gmail.com>
2025-02-13 21:15:17 +0530

    adding first sample file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
- // test comment
+
 function App() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleButtonClick = () => {
+    alert('Button clicked!');
+  };
+
+  const handleDropdownChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +28,13 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={handleButtonClick}>Click Me</button>
+        <select value={selectedOption} onChange={handleDropdownChange}>
+          <option value="">Select an option</option>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
       </header>
     </div>
   );
