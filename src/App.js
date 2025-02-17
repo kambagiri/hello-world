8bd2bfcb5f0b5a128a180553879ba5efbb8b3727
Kambagiri-Atte_volvo <kambagiri.atte@volvo.com>
2025-02-17 22:19:52 +0530

    new world part-2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New World !!
+          New World part-2 !!
         </p>
         <a
           className="App-link"
